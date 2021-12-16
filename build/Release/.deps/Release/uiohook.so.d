97d5e5b24434edb97953844324e9d0569be483d1
Abhishek Cherath <abhicherath@gmail.com>
2021-12-16 15:17:54 -0500

    remove build artifacts

--- build/Release/.deps/Release/uiohook.so.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/uiohook.so := ln -f "Release/obj.target/uiohook.so" "Release/uiohook.so" 2>/dev/null || (rm -rf "Release/uiohook.so" && cp -af "Release/obj.target/uiohook.so" "Release/uiohook.so")
