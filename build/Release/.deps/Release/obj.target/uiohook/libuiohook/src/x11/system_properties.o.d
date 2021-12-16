664f3af7f63395a396024dd6a9616cb4b5bde477
Abhishek Cherath <abhicherath@gmail.com>
2021-12-16 15:17:20 -0500

    remove install from package, inspired by robotjs

---
+++ build/Release/.deps/Release/obj.target/uiohook/libuiohook/src/x11/system_properties.o.d
@@ -0,0 +1,9 @@
+cmd_Release/obj.target/uiohook/libuiohook/src/x11/system_properties.o := cc -o Release/obj.target/uiohook/libuiohook/src/x11/system_properties.o ../libuiohook/src/x11/system_properties.c '-DNODE_GYP_MODULE_NAME=uiohook' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DV8_COMPRESS_POINTERS' '-DV8_COMPRESS_POINTERS_IN_ISOLATE_CAGE' '-DV8_31BIT_SMIS_ON_64BIT_ARCH' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DUSE_XKBCOMMON' -I/home/abhishek/.cache/node-gyp/17.1.0/include/node -I/home/abhishek/.cache/node-gyp/17.1.0/src -I/home/abhishek/.cache/node-gyp/17.1.0/deps/openssl/config -I/home/abhishek/.cache/node-gyp/17.1.0/deps/openssl/openssl/include -I/home/abhishek/.cache/node-gyp/17.1.0/deps/uv/include -I/home/abhishek/.cache/node-gyp/17.1.0/deps/zlib -I/home/abhishek/.cache/node-gyp/17.1.0/deps/v8/include -I../node_modules/nan -I../libuiohook/include -I../libuiohook/src  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -std=c++14 -fPIC -O3 -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/uiohook/libuiohook/src/x11/system_properties.o.d.raw   -c
+Release/obj.target/uiohook/libuiohook/src/x11/system_properties.o: \
+ ../libuiohook/src/x11/system_properties.c \
+ ../libuiohook/include/uiohook.h ../libuiohook/src/x11/input_helper.h \
+ ../libuiohook/src/logger.h
+../libuiohook/src/x11/system_properties.c:
+../libuiohook/include/uiohook.h:
+../libuiohook/src/x11/input_helper.h:
+../libuiohook/src/logger.h:
